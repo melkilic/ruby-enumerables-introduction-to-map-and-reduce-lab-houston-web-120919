5b08796db19ce197a1b0d97335f164376afefff8
Melike KILIC <melike180400@gmail.com>
2019-10-25 17:18:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,68 @@
 # My Code here....
+def map_to_negativize(array)
+  new_array=[]
+  counter=0
+  while counter< array.length do
+    new_array.push(array[counter]* -1)
+    counter +=1
+  end
+  new_array
+end
+
+def map_to_no_change(array)
+  new_array=[]
+  counter=0
+  while counter< array.length do
+    new_array.push(array[counter])
+    counter +=1
+  end
+  new_array
+end
+
+def map_to_double(array)
+  new_array=[]
+  counter=0
+  while counter< array.length do
+    new_array.push(array[counter]* 2)
+    counter +=1
+  end
+  new_array
+end
+
+def map_to_square(array)
+  new_array=[]
+  counter=0
+  while counter< array.length do
+    new_array.push(array[counter]** 2)
+    counter +=1
+  end
+  new_array
+end
+
+def reduce_to_total(array,starting_point=0)
+  total=starting_point
+  counter=0
+  while counter< array.length do
+  total += array[counter]
+    counter +=1
+  end
+total
+end
+
+def reduce_to_all_true(array)
+  counter=0
+  while counter< array.length
+    return false if nil
+    counter +=1
+  end
+return true
+end
+
+def reduce_to_any_true(array)
+  counter=0
+  while counter< array.length
+    return true if ""
+    counter +=1
+  end
+return false
+end
